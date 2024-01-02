c990374817cc126634f23744c0e0619299ea5988
Aguchukwu Emmanuel <therevealerdev@gmail.com>
2024-01-02 22:33:15 +0100

    Completed Queue implementation

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,43 @@
-class Queue {}
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.head = 0;
+        this.tail = 0;
+        this.maxsize = 100;
+    }
+
+    getLength(){
+        return this.tail - this.head;
+    }
+
+    isEmpty(){
+        return this.getLength() === 0;
+    }
+
+    isFull(){
+        return this.getLength() === this.maxsize;
+    }
+
+    enqueue(element){
+
+        if(this.isFull()){
+            return false;
+        } else{
+            this.queue[this.tail] = element;
+            this.tail++
+            return true;
+        }
+    }
+
+    dequeue(){
+        const item = this.queue[this.head];
+        this.head++;
+      return item;
+    }
+
+    peek() {
+        return this.queue[this.head];
+    }
+}
 
 module.exports = Queue;
