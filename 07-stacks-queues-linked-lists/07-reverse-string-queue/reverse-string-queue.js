7f50d481e4dabe4bf8cb18f06e566f46a691ab2e
Aguchukwu Emmanuel <officialnfluence@gmail.com>
2024-01-06 00:37:25 +0100

    relearning how to reverse a string

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,19 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+    const queue = new Queue();
+
+   for (let i = str.length - 1; i <= 0; i--){
+     queue.enqueue(str[i]) 
+   }
+
+  const reversedString = ""
+  while(queue.length > 0){
+    reversedString+= queue.dequeue()
+  }
+
+  return reversedString;
+
+};
 
 module.exports = reverseStringWithQueue;
