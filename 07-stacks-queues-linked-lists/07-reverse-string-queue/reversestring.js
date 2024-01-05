c8e5c11c78d97e973fa83643df1289a395143557
Aguchukwu Emmanuel <officialnfluence@gmail.com>
2024-01-05 23:56:49 +0100

    relearning how to reverse a string

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reversestring.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reversestring.js
@@ -1,9 +1,27 @@
-// Using in
+// Using inbuilt functions
+
 function reverseString (str) { 
     return str.split("").reverse().join("");
 }
 
 let test = " hello world"
 let result = reverseString(test);
+
+console.log(result)
+
+
+//Using a loop
+function reverseStr(str){
+let reversed = ""
+
+    for(let i = str.length - 1; i >= 0; i--){
+    reversed += str[i];
+    }
+
+    return reversed;
+}
+
+let testing = " hello world"
+let results = reverseStr(test);
 
 console.log(result)
