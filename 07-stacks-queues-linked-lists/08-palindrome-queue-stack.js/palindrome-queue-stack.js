d3853fc2aeaf52684d59da917048418f1862b431
Aguchukwu Emmanuel <officialnfluence@gmail.com>
2024-01-06 01:00:32 +0100

    Palindrome with stack and queues

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,23 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+    const cleanedStr = str.toLowerCase().replace(/[\W_]/g, '');
+    
+    const stack = new Stack();
+    const queue = new Queue();
+
+    for (let i = str.length - 1; i <= 0; i--){
+        queue.enqueue(str[i]) 
+    }
+
+    const reversedString = "";
+    while(!queue.isEmpty()){
+    reversedString+= queue.dequeue()
+    }
+
+
+
+}
 
 module.exports = isPalindromeQueueStack;
